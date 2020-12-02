a6e7a2ec5e591bcafd60bc5c5422431b381c8327
abdul <abdul.tanveer@gmail.com>
2020-12-02 16:23:17 +0530

    removednight mode theme

--- app/src/main/java/com/next/myapplication/CardPresenter.java
+++ app/src/main/java/com/next/myapplication/CardPresenter.java
@@ -1,5 +1,6 @@
 package com.next.myapplication;
 
+import android.graphics.LinearGradient;
 import android.graphics.drawable.Drawable;
 
 import androidx.leanback.widget.ImageCardView;
@@ -7,7 +8,9 @@ import androidx.leanback.widget.Presenter;
 import androidx.core.content.ContextCompat;
 
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
 import com.next.myapplication.Movie;
@@ -50,14 +53,14 @@ public class CardPresenter extends Presenter {
         mDefaultCardImage = ContextCompat.getDrawable(parent.getContext(), R.drawable.movie);
 
         ImageCardView cardView =
-                new ImageCardView(parent.getContext()) {
+                new ImageCardView(parent.getContext() ) {
                     @Override
                     public void setSelected(boolean selected) {
                         updateCardBackgroundColor(this, selected);
                         super.setSelected(selected);
                     }
                 };
-
+        //cardView.setLayoutParams( new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         cardView.setFocusable(true);
         cardView.setFocusableInTouchMode(true);
         updateCardBackgroundColor(cardView, false);
